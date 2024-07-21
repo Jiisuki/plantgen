7d2c25a87943a9e7bcee04e0593f27b6cebde865
dge_andre_lundkvist <andre.lundkvist@greenworkstools.eu>
2024-07-21 08:40:30 +0200

    impl-child: Some cleanup for handling compilation default values.

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -12,11 +12,35 @@
 
 void configure_default(WriterConfig& cfg, std::string& out)
 {
+#ifdef DEFAULT_LONG_NAMES
+    cfg.use_simple_names = false;
+#else
     cfg.use_simple_names = true;
+#endif
+
+#ifdef DEFAULT_VERBOSE
+    cfg.verbose = true;
+#else
     cfg.verbose = false;
+#endif
+
+#ifdef DEFAULT_TRACING
+    cfg.do_tracing = true;
+#else
     cfg.do_tracing = false;
+#endif
+
+#ifdef DEFAULT_CHILD_FIRST
+    cfg.parent_first_execution = false;
+#else
     cfg.parent_first_execution = true;
+#endif
+
+#ifdef DEFAULT_OUTPUT
+    out = DEFAULT_OUTPUT;
+#else
     out = "src/src-gen";
+#endif
 }
 
 void print_usage()
@@ -30,11 +54,31 @@ void print_usage()
     std::cout << "\t-o <folder>\tWhere to store the generated files" << std::endl;
     std::cout << "\t-i <file>\tWhat file to generate" << std::endl << std::endl;
     std::cout << "\tDefault values:" << std::endl;
+#ifdef DEFAULT_LONG_NAMES
+    std::cout << "\t\tLong state names: enabled" << std::endl;
+#else
     std::cout << "\t\tLong state names: disabled" << std::endl;
+#endif
+#ifdef DEFAULT_VERBOSE
+    std::cout << "\t\tVerbose output:   enabled" << std::endl;
+#else
     std::cout << "\t\tVerbose output:   disabled" << std::endl;
+#endif
+#ifdef DEFAULT_TRACING
+    std::cout << "\t\tGenerate tracing: enabled" << std::endl;
+#else
     std::cout << "\t\tGenerate tracing: disabled" << std::endl;
+#endif
+#ifdef DEFAULT_CHILD_FIRST
+    std::cout << "\t\tChild first exec: enabled" << std::endl;
+#else
     std::cout << "\t\tChild first exec: disabled" << std::endl;
+#endif
+#ifdef DEFAULT_OUTPUT
+    std::cout << "\t\tOutput folder:    " << DEFAULT_OUTPUT << std::endl;
+#else
     std::cout << "\t\tOutput folder:    src/src-gen" << std::endl;
+#endif
 }
 
 int parse_arguments(int argc, char* argv[], WriterConfig& cfg, std::string& in, std::string& out)
@@ -106,8 +150,8 @@ int parse_arguments(int argc, char* argv[], WriterConfig& cfg, std::string& in, 
 int main(int argc, char* argv[])
 {
     WriterConfig cfg {};
-    std::string filename {};
-    std::string outdir {};
+    std::string  filename {};
+    std::string  outdir {};
 
     configure_default(cfg, outdir);
     if (0 != parse_arguments(argc, argv, cfg, filename, outdir))
